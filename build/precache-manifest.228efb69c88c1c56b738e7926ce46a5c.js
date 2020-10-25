8f958c05c87c3ab212e100777c45aba7e632b0d3
cccccyy <1518856229@qq.com>
2020-10-25 17:56:08 +0800

    taobao

---
+++ build/precache-manifest.228efb69c88c1c56b738e7926ce46a5c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0baa66941f3aea11c6c3a0f4cf4407",
+    "url": "/react-taobao/build/index.html"
+  },
+  {
+    "revision": "77282a91d08c0537a50e",
+    "url": "/react-taobao/build/static/css/2.3786585f.chunk.css"
+  },
+  {
+    "revision": "77282a91d08c0537a50e",
+    "url": "/react-taobao/build/static/js/2.811c5eda.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/react-taobao/build/static/js/2.811c5eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ce60bdc3096c272316",
+    "url": "/react-taobao/build/static/js/main.347a3125.chunk.js"
+  },
+  {
+    "revision": "092a489f2239d1b83a66",
+    "url": "/react-taobao/build/static/js/runtime-main.b6b9f60a.js"
+  },
+  {
+    "revision": "37a16a47bc64d6bfdd559c527a88aa93",
+    "url": "/react-taobao/build/static/media/1.37a16a47.webp"
+  },
+  {
+    "revision": "0e7c0b7f67100511ac7ad6ea8b5dbd07",
+    "url": "/react-taobao/build/static/media/10.0e7c0b7f.webp"
+  },
+  {
+    "revision": "48b7d31d69b103e72392db3ed8e323f4",
+    "url": "/react-taobao/build/static/media/2.48b7d31d.webp"
+  },
+  {
+    "revision": "fa250acc5e6173e1698c6698b0575c43",
+    "url": "/react-taobao/build/static/media/3.fa250acc.webp"
+  },
+  {
+    "revision": "ceab73bd0cff7473844a25399f118892",
+    "url": "/react-taobao/build/static/media/4.ceab73bd.webp"
+  },
+  {
+    "revision": "5896f6754cf2eec11bfc00a7471421f6",
+    "url": "/react-taobao/build/static/media/5.5896f675.webp"
+  },
+  {
+    "revision": "3745453860ffe0c4045518af0d0965a3",
+    "url": "/react-taobao/build/static/media/6.37454538.webp"
+  },
+  {
+    "revision": "e4e9777f130c49e27ec88ddf4255ce10",
+    "url": "/react-taobao/build/static/media/7.e4e9777f.webp"
+  },
+  {
+    "revision": "22d9e8af0c9ef1ed4818fe5f0f179372",
+    "url": "/react-taobao/build/static/media/8.22d9e8af.webp"
+  },
+  {
+    "revision": "7b74c3c15643e24f109b42f9f2e003e2",
+    "url": "/react-taobao/build/static/media/9.7b74c3c1.webp"
+  }
+]);
